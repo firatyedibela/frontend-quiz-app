9dcda97c510cf830b28d84e334a52c9a726d5f3d
firatyedibela <yedibelafirat0@gmail.com>
2025-03-09 18:11:14 +0300

    get starter files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>Hello</li>
+        <li>World</li>
+        <li>Today</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
